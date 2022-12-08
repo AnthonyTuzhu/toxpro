1ea1cc8103cd53e7d02527d85556187b1f15738a
Russo <russod@rowan.edu>
2022-12-08 14:18:56 -0500

    more commits

---
+++ app/db_models.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    last_login = db.Column(db.DateTime)
+    user_created = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
