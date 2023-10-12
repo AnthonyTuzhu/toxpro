c2684293e51b8872477a9f2021d6f410cdbdde4c
AnthonyTuzhu <103622321+AnthonyTuzhu@users.noreply.github.com>
2023-10-12 14:46:17 -0500

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source venv/bin/activate
-exec gunicorn -b :5000 --worker-tmp-dir /dev/shm --workers=2  --access-logfile - --error-logfile - "app:create_app()"
+exec gunicorn -b :5000 --worker-tmp-dir /dev/shm --workers=2 --timeout 0  --access-logfile - --error-logfile - "app:create_app()"
